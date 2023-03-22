bad5499ba1cd4e0b3e9fcb6473c56c5d9ddb5f31
Sown13 <sonnh.buh@gmail.com>
2023-03-22 14:45:49 +0700

    update main

---
+++ out/production/practice_java_basic_1/README.md
@@ -0,0 +1,1 @@
+Test first java project
---
+++ src/FirstPractice.java
@@ -0,0 +1,5 @@
+public class FirstPractice{
+    public static void main(String[] args){
+        System.out.println("thu in ra man hinh terminal");
+    }
+}
